2370eed254104dbba07c5acaad6cec0a61f4738e
Djey013 <97234391+Djey013@users.noreply.github.com>
2022-04-22 16:47:10 +0200

    transition & built settings OK

---
+++ Assets/Script/Transition_State.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition_State : MonoBehaviour
+{
+    
+    
+    
+    public void TransitionMenu()
+    {
+        SceneManager.LoadScene("Level02");
+
+    }
+}
